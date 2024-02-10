07fb85480f4b6fd3f68aa03476db8e8149b5d050
shinbumjun <https://github.com/shinbumjun/13_SpringJdbc.git>
2024-02-10 19:54:08 +0900

    2. 로그인 페이지 만들기 - 쿠키와 세션, redirect와 forward 사용하기

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -1,12 +1,47 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
+
+<!-- 메서드 영역 -->
+<% 
+	System.out.println("Controller에서 저장한 세션값 jsp에서 확인해보기 : " + session.getAttribute("memberId"));
+%> 
+
 <html>
 <head>
-<meta charset="EUC-KR">
-<title>Insert title here</title>
+  <meta charset="UTF-8">
+  <title>13_SpringJdbc</title>
+  <link rel="stylesheet" href="<c:url value='/resources/css/menu.css'/>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
 </head>
 <body>
-index.jsp
+<div id="menu">
+  <ul>
+    <li id="logo">bumjun</li>
+    <li><a href="">홈</a></li>
+    <li><a href="">게시판</a></li>
+    
+    
+    <!-- 로그인일 했을때는 > 로그아웃, 로그아웃을 누르면 > 로그인 = session을 이용해서 해보자  -->
+    <!-- li><a href="/spring/index">홈</a></li -->
+    <!-- li><a href="/spring/loginPage">로그인</a></li -->
+    <!-- 세션에 "id"가 존재하면 로그인 상태 -->
+    <c:if test="${not empty sessionScope.memberId}">
+    	<li><a href="/spring/logout">logout</a></li>
+    	<li><c:out value="${sessionScope.memberId}" /></li>
+	</c:if>
+
+    <!-- 세션에 "id"가 존재하지 않으면 로그아웃 상태 -->
+    <c:if test="${empty sessionScope.memberId}">
+    	<li><a href="/spring/loginPage">로그인</a></li>
+	</c:if>
+    
+  </ul>
+</div>
+<div style="text-align:center">
+  <h1>홈입니다</h1>
+
+</div>
 </body>
 </html>
---
+++ src/com/feb/test/controller/LoginController.java
@@ -0,0 +1,76 @@
+package com.feb.test.controller;
+
+import java.net.URLEncoder;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+	// 5. 로그아웃
+	@GetMapping("/logout")
+	public String login(HttpSession session) {
+		// 로그아웃 버튼을 누르면 세션을 종료
+		session.invalidate();
+		// 홈으로 이동
+		return "redirect:/index";
+	}
+	
+	@GetMapping("/loginPage")
+    public String loginForm() {
+        return "login"; // 뷰 이름을 반환합니다.
+    }
+	
+	@PostMapping("/login.do")
+	public String login(String memberId, String passwd, String logCheck, String toURL, HttpServletRequest request,HttpServletResponse response) throws Exception {
+		// 1. Login에 입력한 값이 잘 들어왔는지 확인
+		System.out.println("memberId : " + memberId);
+		System.out.println("passwd : " + passwd);
+		System.out.println("logCheck : " + logCheck); // 체크하면 on, 체크안하면 null
+		
+		// 2. id와 pwd 일치하지 않을 경우
+		if(!(loginCheck(memberId, passwd))) {
+			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
+			return "redirect:/loginPage"; // redirect
+		}
+		
+		// 3. memberId, passwd가 일치하면 세션 객체를 얻어오기
+		HttpSession session = request.getSession();
+		session.setAttribute("memberId", memberId); // 매개변수로 받은 memberId값을 "memberId"키 값으로 저장
+		System.out.println("session.getMemberId() : " + session.getAttribute("memberId")); // "id"키값이 저장 되었는지 찍어보기
+		
+		// 4. 쿠키 생성하기
+		// if(logCheck.equals("on")) { // 체크박스 클랙 o, ***** logCheck=null일 경우  NullPointerException 
+		if("on".equals(logCheck)) {
+			System.out.print("쿠키 저장");
+			// 쿠키 생성
+			Cookie cookie = new Cookie("memberId", memberId);
+			// 응답에 저장
+			response.addCookie(cookie);
+		}else { // 체크박스 클릭 x
+			// 쿠키 삭제
+			Cookie cookie = new Cookie("memberId",memberId);
+			cookie.setMaxAge(0);
+			// 응답에 저장
+			response.addCookie(cookie);
+		}
+
+		return "redirect:/index"; // 로그인 성공할 경우 홈페이지로 이동
+	}
+
+	private boolean loginCheck(String memberId, String passwd) {
+//		if(id=="s123s123s" && pwd == "12345") { ... // *****== 이것으로 비교했을때 if(ture)문이 실행이 안되어서 .equals()로 수정
+		boolean ok = ("s123s123s".equals(memberId) && "123".equals(passwd));
+
+		System.out.println("로그인 확인 : " + ok);
+		return ok;
+	}
+	
+}
